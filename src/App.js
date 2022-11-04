d62a8cd66b3555656cf67436bfadc1c08bdb8af3
gohantabeta <koutanakagawa.kame@outlook.jp>
2022-11-04 21:25:48 +0900

    jpg up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import icon from './icon.jpg';
 import './App.css';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <header>
+        <div className='icon_wrapper'>
+        <a href='./App.js'>
+        <img src={icon} className='img' alt='icon'/>
         </a>
+        </div>
+        <nav className='right_content'>
+      <a href='./App.js'>Home</a>
+      <a href='./index.js'>About</a>
+    </nav>
       </header>
+      <main className='wrapper'>
+        <h1 className='title'>gohantabeta</h1>
+        <p className='desc'>gohantabetaのホームページへようこそ！</p>
+        <div className='social'>
+          <a href='https://github.com/gohantabeta' target='_blank' className='git'>
+            <i className='fa-brands fa-github fa-lg'></i>
+          </a>
+          <a href='https://twitter.com/gohantabetemita' target='_blank' className='twt'>
+        <i className='fa-brands fa-twitter fa-lg'></i>
+      </a>
+        </div>
+      </main>
+      <footer>
+        <p>&copy; 2022. gohantabeta. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
