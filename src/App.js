1d52d542b33139e70d04bddd64e625e4e83f0272
gohantabeta <koutanakagawa.kame@outlook.jp>
2022-11-27 18:12:41 +0900

    conpair url

--- src/App.js
+++ src/App.js
@@ -14,29 +14,29 @@ function App() {
         <BrowserRouter>
         <header>
         <div className='icon_wrapper'>
-        <Link to="/Home" className='a'>
+        <Link to="/React-GH-Pages/Home" className='a'>
         <img src={icon} className='img' alt='icon'/>
         </Link>
         </div>
         <nav className='right_content'>
-        <Link to="/Home" className='a'>Home</Link>
+        <Link to="/React-GH-Pages/Home" className='a'>Home</Link>
         <br />
-        <Link to="/About" className='a'>About</Link>
+        <Link to="/React-GH-Pages/About" className='a'>About</Link>
         <br />
-        <Link to="/More" className='a'>More</Link>
+        <Link to="/React-GH-Pages/More" className='a'>More</Link>
         <br />
         </nav>
         </header>
 
         <main>
         <Routes>
-          <Route path="/Home" element={<Home />}>
+          <Route path="/React-GH-Pages/Home" element={<Home />}>
           </Route>
           <Route path="/React-GH-Pages" element={<Home />}>
           </Route>
-          <Route path="/About" element={<About />}>
+          <Route path="/React-GH-Pages/About" element={<About />}>
           </Route>
-          <Route path="/More" element={<More />}>
+          <Route path="/React-GH-Pages/More" element={<More />}>
           </Route>
           <Route path="*" element={<Page404 />}>
           </Route>
